553d29babadde4b14cb00561dbe9466016c86367
Dodda Srinivasan <srinivasan.munna@gmail.com>
2016-08-26 12:35:20 +0530

    Initial Commit

---
+++ ScrollEffect/ScrollEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollEffect
+//
+//  Created by Srinivasan Dodda on 26/08/16.
+//  Copyright © 2016 srini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollEffect/ScrollEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollEffect
+//
+//  Created by Srinivasan Dodda on 26/08/16.
+//  Copyright © 2016 srini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
